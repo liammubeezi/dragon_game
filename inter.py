e9c36b0e81402d86fa0e4a75142ea6b59f8ac964
LIAM CHRISTIAN <liamchristian88@gmail.com>
2024-12-16 20:03:40 +0300

    sounds

--- inter.py
+++ inter.py
@@ -7,6 +7,7 @@ import level
 WIDTH, HEIGHT = 1500, 800
 
 pygame.font.init()
+pygame.mixer.init()
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First, Game!")
@@ -49,6 +50,8 @@ TARGET = pygame.transform.scale(TARGET, (TARGET_WID, TARGET_HEI))
 text_font = pygame.font.SysFont("Helvetica", 100)
 
 boundary = "You Lose"
+fire_sound = pygame.mixer.Sound("dragon_assets/Grenade+1.mp3")
+collide_sound=pygame.mixer.Sound("dragon_assets/Gun+Silencer.mp3")
 
 def new():
     dragon = pygame.Rect(DRAG_HORI, DRAG_VERT, DRAG_WID, DRAG_HEI)
@@ -71,6 +74,7 @@ def new():
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_f:
+                    fire_sound.play()
                     if rotation == 270:
                         fire = pygame.Rect(dragon.x, dragon.y + DRAG_HEI//2, 30, 10)    #fix fire launch origin
                         dragon_fire.append(('left', fire))
